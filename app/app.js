ac9049bf4cab6255412f0bdf9d32953d8fd7d20c
Steven Broderick <brodericksteven@gmail.com>
2016-01-23 21:47:46 -0800

    Initial commit. Add comment form

---
+++ app/app.js
@@ -0,0 +1,34 @@
+Comments = new Mongo.Collection('comments');
+
+if (Meteor.isClient) {
+  Template.commentList.helpers({
+    comments: function() {
+      return Comments.find();
+    }
+  });
+
+  Template.commentAdd.events({
+    'submit form': function(e, template) {
+      e.preventDefault();
+
+      var form = template.find('form'),
+          commentField = template.find('[name=comment]'),
+          commentText = commentField.value;
+
+      Comments.insert({
+        login: 'stevula',
+        timestamp: new Date(),
+        room: 'main',
+        comment: commentText
+      });
+
+      form.reset();
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+// code to run on server at startup
+});
+}
